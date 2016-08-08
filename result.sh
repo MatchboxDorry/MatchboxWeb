2e80e5f4d836015c727f3fb5ffa99b2ce4c1e676
MatchboxDorry <job@dorry.io>
2016-08-08 10:00:03 +0800

    delete the index.html joinus.html

--- result.sh
+++ result.sh
@@ -2,9 +2,11 @@
 grunt
 # checkout the status
 git status
-# add the changed files to the develop branch
+# git add the changed files to the develop branch
 read -p "Enter files name you need to add :" name1 name2 name3 name4 name5
 git add $name1 $name2 $name3 $name4 $name5
-# commit the add files to the develop branch
-read -p "Enter files name you need to add :"
+# git commit the add files to the develop branch
+read -p "Enter commit message:" message
 git commit -m "$message"
+# git change branch to test
+git checkout test
