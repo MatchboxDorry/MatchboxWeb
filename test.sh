01e5df93f0aac9efd07c7d0b81e187533df99fdc
MatchboxDorry <job@dorry.io>
2016-08-10 17:53:56 +0800

    delete no using files

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # Clear all the files except test.sh
-git rm -r assets *.html
+git rm -r assets *.html 
 # checkout product from develop branch
 git checkout develop product
 # git mv all the files and directories from product to current path
